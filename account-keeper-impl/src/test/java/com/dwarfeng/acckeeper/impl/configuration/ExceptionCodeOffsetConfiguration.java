d152079288dcbc2936099be1732edfde3289b026
DwArFeng <915724865@qq.com>
2020-02-12 23:33:32 +0800

    ExceptionCode偏移量可配置化
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ account-keeper-impl/src/test/java/com/dwarfeng/acckeeper/impl/configuration/ExceptionCodeOffsetConfiguration.java
@@ -0,0 +1,25 @@
+package com.dwarfeng.acckeeper.impl.configuration;
+
+import com.dwarfeng.acckeeper.sdk.util.ServiceExceptionCodes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class ExceptionCodeOffsetConfiguration {
+
+    @Value("${acckeeper.exception_code_offset}")
+    private int exceptionCodeOffset;
+    @Value("${acckeeper.exception_code_offset.subgrade}")
+    private int subgradeExceptionCodeOffset;
+    @Value("${acckeeper.exception_code_offset.snowflake}")
+    private int snowflakeExceptionCodeOffset;
+
+    @PostConstruct
+    public void init() {
+        ServiceExceptionCodes.setExceptionCodeOffset(exceptionCodeOffset);
+        com.dwarfeng.subgrade.sdk.exception.ServiceExceptionCodes.setExceptionCodeOffset(subgradeExceptionCodeOffset);
+        com.dwarfeng.subgrade.sdk.exception.ServiceExceptionCodes.setExceptionCodeOffset(snowflakeExceptionCodeOffset);
+    }
+}
--- account-keeper-sdk/src/main/java/com/dwarfeng/acckeeper/sdk/util/ServiceExceptionCodes.java
+++ account-keeper-sdk/src/main/java/com/dwarfeng/acckeeper/sdk/util/ServiceExceptionCodes.java
@@ -2,8 +2,6 @@ package com.dwarfeng.acckeeper.sdk.util;
 
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 
-import static com.dwarfeng.acckeeper.sdk.util.Constants.EXCEPTION_CODE_OFFSET;
-
 /**
  * 服务异常代码。
  *
@@ -11,6 +9,8 @@ import static com.dwarfeng.acckeeper.sdk.util.Constants.EXCEPTION_CODE_OFFSET;
  * @since 0.0.1-alpha
  */
 public final class ServiceExceptionCodes {
+
+    private static int EXCEPTION_CODE_OFFSET = 1000;
 
     /**
      * 账户已经存在。
@@ -32,6 +32,24 @@ public final class ServiceExceptionCodes {
      * 登录过期。
      */
     public static final ServiceException.Code LOGIN_OUTDATED = new ServiceException.Code(EXCEPTION_CODE_OFFSET + 40, "login outdated");
+
+    /**
+     * 获取异常代号的偏移量。
+     *
+     * @return 异常代号的偏移量。
+     */
+    public static int getExceptionCodeOffset() {
+        return EXCEPTION_CODE_OFFSET;
+    }
+
+    /**
+     * 设置异常代号的偏移量。
+     *
+     * @param exceptionCodeOffset 指定的异常代号的偏移量。
+     */
+    public static void setExceptionCodeOffset(int exceptionCodeOffset) {
+        EXCEPTION_CODE_OFFSET = exceptionCodeOffset;
+    }
 
     private ServiceExceptionCodes() {
         throw new IllegalStateException("禁止实例化");
