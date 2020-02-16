72b096f81c547bed1689d00fb7a6f5f412319179
DwArFeng <915724865@qq.com>
2020-02-16 21:24:17 +0800

    优化AccountPresetCriteriaMaker的查询参数
    Signed-off-by: DwArFeng <915724865@qq.com>

--- account-keeper-impl/src/main/java/com/dwarfeng/acckeeper/impl/dao/preset/AccountPresetCriteriaMaker.java
+++ account-keeper-impl/src/main/java/com/dwarfeng/acckeeper/impl/dao/preset/AccountPresetCriteriaMaker.java
@@ -2,8 +2,10 @@ package com.dwarfeng.acckeeper.impl.dao.preset;
 
 import com.dwarfeng.acckeeper.stack.service.AccountMaintainService;
 import com.dwarfeng.subgrade.sdk.hibernate.criteria.PresetCriteriaMaker;
+import com.dwarfeng.subgrade.stack.bean.key.StringIdKey;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Component;
 
@@ -29,8 +31,9 @@ public class AccountPresetCriteriaMaker implements PresetCriteriaMaker {
 
     private void idLike(DetachedCriteria criteria, Object[] objs) {
         try {
-            String stringId = (String) objs[0];
-            criteria.add(Restrictions.like("stringId", stringId, MatchMode.ANYWHERE));
+            StringIdKey stringIdKey = (StringIdKey) objs[0];
+            criteria.add(Restrictions.like("stringId", stringIdKey.getStringId(), MatchMode.ANYWHERE));
+            criteria.addOrder(Order.asc("stringId"));
         } catch (Exception e) {
             throw new IllegalArgumentException("非法的参数:" + Arrays.toString(objs));
         }
