4db74e20e1f2db2f012ac7273df45b20fd209d23
DwArFeng <915724865@qq.com>
2020-04-10 10:36:51 +0800

    优化程序的启动脚本
    Signed-off-by: DwArFeng <915724865@qq.com>

--- account-keeper-node/src/assembly/acckeeper-start.sh
+++ account-keeper-node/src/assembly/acckeeper-start.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # 程序的根目录
-basedir=/usr/share/acckeeper
+basedir=/usr/local/acckeeper
 # 日志的根目录
 logdir=/var/log/acckeeper
-# 可执行的jar名称
-executable_jar_name=${project.name}-${project.version}.jar
 
 cd $basedir || exit
-nohup /bin/java -Dlog.dir=$logdir -jar $basedir/lib/$executable_jar_name >/dev/null 2>&1 &
+# shellcheck disable=SC2154
+# shellcheck disable=SC2086
+nohup /bin/java -classpath "lib/*:libext/*" \
+-Dlog.dir=$logdir \
+-Dlog.consoleEncoding=UTF-8 \
+-Dlog.fileEncoding=UTF-8 \
+${mainClass} \
+>/dev/null 2>&1 &
 echo $! >$basedir/acckeeper.pid
--- account-keeper-node/src/assembly/acckeeper-stop.sh
+++ account-keeper-node/src/assembly/acckeeper-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 程序的根目录
-basedir=/usr/share/acckeeper
+basedir=/usr/local/acckeeper
 
 PID=$(cat $basedir/acckeeper.pid)
 kill "$PID"
